76b7655f48527c8678b7f34edf7a56a193b19502
Jonas Waage <jonas.h.waage@gmail.com>
2020-06-26 00:24:39 +0200

    Add docs

--- src/main/kotlin/simple-peer.kt
+++ src/main/kotlin/simple-peer.kt
@@ -10,12 +10,27 @@ import kotlin.random.Random
 
 @JsModule("simple-peer")
 @JsNonModule
+/**
+ * WebRTC peer
+ * @param configuration peer configuration
+ */
 external class Peer(configuration: Configuration = definedExternally) {
 
+    /**
+     * Attach handlers to named events
+     * @param event the event
+     * @param handle callback on event
+     */
     fun on(event : String,handle : (data :dynamic) -> Unit)
 
+    /**
+     * @param data data to send
+     */
     fun send(data : String)
 
+    /**
+     * @param signal received signal
+     */
     fun signal(signal : dynamic)
 }
 
@@ -34,10 +49,31 @@ fun getRandomChar() :Char {
     return HEX_LETTERS[Random.nextInt(HEX_LETTERS.length)]
 }
 
+/**
+ * @param urls stun or turn urls
+ */
 data class IceEntry(val urls : String)
 
+/**
+ * @param iceServers stun and turn servers
+ */
 data class Config(val iceServers: Array<IceEntry> = emptyArray())
 
+/**
+ * Peer configuration
+ *
+ * @param initiator whether this peer initiates the connection
+ * @param channelName name of channel
+ * @param channelConfig data channel config (unordered/ordered) and so o
+ * @param offerOptions ?
+ * @param answerOptions ?
+ * @param sdpTransform munging
+ * @param streams ?
+ * @param trickle use trickle ice
+ * @param allowHalfTrickle ?
+ * @param wrtc ?
+ * @param objectMode ?
+ */
 data class Configuration(
         val initiator : Boolean = false,
         val channelName : String = randomStringOfLength(8),
